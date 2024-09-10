ff5fa16b34c9d5f16a9d1ea65e5a7d0458fb64cd
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-09-10 11:21:55 +0530

    monor change

--- fitnesstrack/src/main.jsx
+++ fitnesstrack/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-import App from "./App";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
