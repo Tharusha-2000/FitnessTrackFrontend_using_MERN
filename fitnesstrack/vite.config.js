379fbae0b9a9377ae427549c7df473037138de35
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-09-10 19:23:18 +0530

    error fix

--- fitnesstrack/vite.config.js
+++ fitnesstrack/vite.config.js
@@ -1,30 +1,30 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-
-// })
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      dayjs: 'dayjs'
-    }
-  },
-  // build: {
-  //   rollupOptions: {
-  //     external: ['react-router-dom'],
-  //   },
-  //   minify: false, // Disable minification
-  //   sourcemap: true,
 
-  // },
-});
+})
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       dayjs: 'dayjs'
+//     }
+//   },
+//   // build: {
+//   //   rollupOptions: {
+//   //     external: ['react-router-dom'],
+//   //   },
+//   //   minify: false, // Disable minification
+//   //   sourcemap: true,
+
+//   // },
+// });
