08c2e26eecaa3eab471406d63d098c4cb33e805c
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-09-10 11:35:34 +0530

    check errors

--- fitnesstrack/src/App.jsx
+++ fitnesstrack/src/App.jsx
@@ -2,14 +2,8 @@ import { ThemeProvider, styled } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Authentication from "./pages/Authentication";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
-import Workouts from "./pages/Workouts";
-import Tutorial from "./components/Tutorial";
-import Blogs from "./components/Blogs";
-import Contact from "./components/Contact";
 
 const Container = styled.div`
   width: 100%;
@@ -27,20 +21,14 @@ function App() {
   const { currentUser } = useSelector((state) => state.user);
   return (
     <ThemeProvider theme={lightTheme}>
-    <BrowserRouter>
-      <Container>
-        {currentUser && <Navbar currentUser={currentUser} />}
-        <Routes>
-          <Route path="/" element={currentUser ? <Dashboard /> : <Authentication />} />
-          <Route path="/workouts" element={<Workouts />} />
-          <Route path="/tutorials" element={<Tutorial/>} />
-          <Route path="/blogs" element={<Blogs/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/" element={<Authentication />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
-  </ThemeProvider>
+      <BrowserRouter>
+        <Container>
+          <Routes>
+            <Route path="/" element={currentUser ? <Dashboard /> : <Authentication />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
