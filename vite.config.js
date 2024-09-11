f2036df7338e537b7fc1dd28d2d6840848f6e22b
tharushadinuth(pro) <gunasekarawtd.21@uom.lk>
2024-09-11 19:35:32 +0530

    vite configer change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-
-})
-
-
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [], // This should be empty unless you want to exclude specific modules
+    },
+  },
+});
 
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
